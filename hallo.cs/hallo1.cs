fb8cf5dc1a635f9fb61482258116e592a882d0ef
gn15kemi <lorelltchapda@gmail.com>
2020-05-02 20:53:40 +0200

    2. commit

---
+++ hallo.cs/hallo1.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+namespace Programm
+{
+	class Hallo
+	{
+		static void Main ()
+		{
+			Console.WriteLine("Hallo Welt");
+			
+
+			Console.WriteLine("enter a number i will give u a square ");
+			Console.ReadKey();	
+
+			}
+	}
+}
